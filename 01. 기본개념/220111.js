9b4f65af52dbde4c7dfb40a7a9e219d15aa3b6c5
alsgurdk22 <39902920+alsgurdk22@users.noreply.github.com>
2022-01-11 22:24:29 +0900

    Update 220111.js

--- 01. 기본개념/220111.js
+++ 01. 기본개념/220111.js
@@ -34,3 +34,80 @@ console.log('a = ' + a + ', b = ' + b);
 // 2.4 조건문
 // 거짓: false, '', 0, NaN, null, undefined
 // 참: 그 외 나머지 보통 true
+
+// 학점 계산 if문
+const score = 90;
+if (score >= 90) {
+  console.log('A');
+} else if (score < 90 && score >= 80) {
+  console.log('B');
+} else if (score < 80 && score >= 70) {
+  console.log('C');
+} else if (score < 70 && score >= 60) {
+  console.log('D');
+} else {
+  console.log('F');
+}
+// 위 코드 줄이기
+// 조건문이 위에서 아래로 차례대로 실행된다는 특성을 이용
+const score2 = 70;
+if (score2 >= 90) {
+  console.log('A');
+} else if (score2 >= 80) {
+  console.log('B');
+} else if (score2 >= 70) {
+  console.log('C');
+} else if (score2 >= 60) {
+  console.log('D');
+} else {
+  console.log('F');
+}
+// if 문 사용시 중첩 if 사용은 피하는 것이 좋다.
+// 코드 가독성에 떨어지므로 최대한 if-else 문으로 변환이 필요함.
+
+// if 문과 switch 문 비교
+let fruit = 'apple';
+if (fruit === 'apple') {
+  console.log('apple!!!');
+} else if (fruit === 'pear') {
+  console.log('pear!!!');
+} else if (fruit === 'grape') {
+  console.log('grape!!!');
+} else {
+  console.log('nothing~');
+}
+
+switch (fruit) {
+  default:
+    console.log('nothing~');
+    break;
+  case 'apple':
+    console.log('apple!!!');
+    break;
+  case 'pear':
+    console.log('pear!!!');
+    break;
+  case 'grape':
+    console.log('grape!!!');
+    break;
+}
+
+// !!삼항연산자!! 최근 많이 쓰는 연산자! 매우 중요하며, '조건부 연산자' 라고도 부름
+// 조건식 ? 참일 때 실행되는 식 : 거짓일 때 실행되는 식
+console.log(5 > 0 ? 'true' : 'false');
+// 조건부 연산은 조건에 따라 달라지는 값을 변수에 대입할 때 사용합니다.
+let val1 = 5 < 0 ? 'true' : 'false';
+console.log(val1);
+
+// if 문과 삼항연산자 비교
+// 삼항연산자가 월등히 짧음
+let condition = true;
+let val2 = condition ? 'true!' : 'false!';
+console.log(val2);
+
+if (condition) {
+  val2 = 'true!';
+} else {
+  val2 = 'false!';
+}
+console.log(val2);
