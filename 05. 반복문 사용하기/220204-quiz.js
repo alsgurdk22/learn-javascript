88599213aa2a1c1a6f5bc8e514a6fcab3fc3c506
alsgurdk22 <39902920+alsgurdk22@users.noreply.github.com>
2022-02-04 13:06:29 +0900

    Create 220204-quiz.js
    반복문의 시작 값과 끝 값을 입력하는 문제

---
+++ 05. 반복문 사용하기/220204-quiz.js
@@ -0,0 +1,30 @@
+// 2 ~ 5까지 숫자 뽑기
+// 아래 반복문이 제대로 작동되게하기
+
+// quiz 1
+const answer = [];
+for (let n = 0; n < 4; n++) {
+  answer.push(n + 2);
+}
+console.log(answer);
+
+// quiz 2
+const answer1 = [];
+for (let n = 0; n <= 3; n++) {
+  answer1.push(n + 2);
+}
+console.log(answer1);
+
+// quiz 3
+const answer2 = [];
+for (let n = 1; n < 5; n++) {
+  answer2.push(n + 1);
+}
+console.log(answer2);
+
+// quiz 4
+const answer3 = [];
+for (let n = 1; n <= 4; n++) {
+  answer3.push(n + 1);
+}
+console.log(answer3);
